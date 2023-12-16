d9e195499a63c4eea06dc1e5dabcb127e05272ab
Prathmesh Karche <karcheprathmesh@gmail.com>
2023-12-16 18:59:33 +0530

    Added Styles to Cart

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -13,19 +13,20 @@ const CartItem = ({item, itemIndex}) => {
   }
 
   return (
-    <div>
+    <div className="flex items-center p-2 md:p-5 justify-between   mt-2 mb-2 md:mx-5 ">
 
-      <div>
+      <div className="flex items-center p-2 md:p-5 justify-between   mt-2 mb-2 md:mx-5 ">
 
-        <div>
-          <img src={item.image} />
+        <div className="w-[30%]">
+          <img src={item.image}/>
         </div>
-        <div>
-          <h1>{item.title}</h1>
-          <h1>{item.description}</h1>
-          <div>
-            <p>{item.price}</p>
-            <div
+        <div className="md:ml-10 self-start space-y-5 w-[100%] md:w-[70%] ">
+          <h1 className="text-xl text-slate-700 font-semibold">{item.title}</h1>
+          {/* post.description.split(" ").slice(0,10).join(" ") + "..." */}
+          <h1 className="text-base text-slate-700 font-medium">{item.description.split(" ").slice(0,10).join(" ")+"..."}</h1>
+          <div className="flex items-center justify-between">
+            <p className="font-bold text-lg text-green-600" >${item.price}</p>
+            <div className=" bg-red-200 group hover:bg-red-400 transition-transform duration-300 cursor-pointer rounded-full p-3 mr-3"
             onClick={removeFromCart}>
               <FcDeleteDatabase/>
             </div>
